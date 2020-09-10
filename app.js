1a2c174125bb1fde499200a78d7fce32d5d37745
vishnu <chilamakurvishnu@gmail.com>
2020-09-10 13:52:11 +0530

    starting up project

---
+++ app.js
@@ -0,0 +1,23 @@
+// ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.querySelector(".grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+// edit option
+let editElement;
+let editFlag;
+let editId = "";
+
+// ****** EVENT LISTENERS **********
+
+// submit form
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
