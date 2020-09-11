17fb5117e85577f420c6fbcb6ba01ddfe063266b
vishnu <chilamakurvishnu@gmail.com>
2020-09-11 09:38:20 +0530

    setBackToDefault function is added

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ let editId = "";
 // ****** EVENT LISTENERS **********
 // submit form
 form.addEventListener("submit", addItem);
+clearBtn.addEventListener("click", clearItems);
 
 // ****** FUNCTIONS **********
 function addItem(e) {
@@ -47,13 +48,19 @@ function addItem(e) {
     // add class list to the child
     list.classList.add("show-container");
     displayAlert("Item added to the list", "success");
+    // show container
+    container.classList.add("show-container");
+    // add to local storage
+    addLocalStorage(id, value);
+    // set back to degault
+    setBackToDefault();
   } else if (value && !editFlag) {
     displayAlert("Item edited Successfully", "success");
   } else {
     displayAlert("Invalid input, Try again", "danger");
   }
 }
-
+// display alert
 function displayAlert(text, action) {
   alert.textContent = text;
   alert.classList.add(`alert-${action}`);
@@ -63,8 +70,19 @@ function displayAlert(text, action) {
     alert.classList.remove(`alert-${action}`);
   }, 1000);
 }
-// display alert
 
+// clear items
+function clearItems() {}
+
+// set back to degault
+function setBackToDefault() {
+  grocery.value = "";
+  editFlag = false;
+  editId = "";
+  submitBtn.textContent = "submit";
+}
 // ****** LOCAL STORAGE **********
-
+function addLocalStorage(id, value) {
+  console.log("added to local storage");
+}
 // ****** SETUP ITEMS **********
